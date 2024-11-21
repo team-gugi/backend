12cdf72b050dce5863bf1192c875c001e22c24a7
susooo <92291198+susooo@users.noreply.github.com>
2024-11-22 01:51:10 +0900

    [Feat] : project settings

---
+++ src/test/java/com/boot/gugi/GugiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.gugi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GugiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
