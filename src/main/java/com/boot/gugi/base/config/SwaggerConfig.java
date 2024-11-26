553eea4f00eaedbf7194704b319316a89b46049d
susooo <92291198+susooo@users.noreply.github.com>
2024-11-26 20:40:44 +0900

    [Fix] : Add Bearer Token authentication to Swagger

--- src/main/java/com/boot/gugi/base/config/SwaggerConfig.java
+++ src/main/java/com/boot/gugi/base/config/SwaggerConfig.java
@@ -1,8 +1,11 @@
 package com.boot.gugi.base.config;
 
+
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,8 +13,17 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
+        SecurityScheme apiKey = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .name("Authorization");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("Bearer Token");
+
         return new OpenAPI()
-                .components(new Components())
+                .components(new Components().addSecuritySchemes("Bearer Token", apiKey))
+                .addSecurityItem(securityRequirement)
                 .info(apiInfo());
     }
     private Info apiInfo() {
