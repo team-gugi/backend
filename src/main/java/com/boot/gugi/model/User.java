16e07b4e2a6a1a99b46515e6312c23891086aa7f
susooo <92291198+susooo@users.noreply.github.com>
2024-11-26 14:09:31 +0900

    [Feat] : User Entity 추가

---
+++ src/main/java/com/boot/gugi/model/User.java
@@ -0,0 +1,41 @@
+package com.boot.gugi.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
+
+@Getter
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(generator = "users_uuid")
+    @GenericGenerator(name="users_uuid", strategy = "uuid2")
+    @Column(columnDefinition = "BINARY(16)", unique = true, nullable = false)
+    private UUID userId;
+
+    @Column(nullable=false)
+    private String provider;
+
+    @Column(nullable=false)
+    private String providerId;
+
+    @Column(nullable=false)
+    private String name;
+
+    @Column(nullable=false)
+    private String email;
+
+    @Column(nullable=false)
+    private Integer gender;
+
+    @Column(nullable=false)
+    private Integer age;
+
+}
