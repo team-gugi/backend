51dd24ff81bf11679743a41274f238a151bf6e3a
susooo <92291198+susooo@users.noreply.github.com>
2024-11-26 17:23:15 +0900

    [Feat] : Add success response codes

---
+++ src/main/java/com/boot/gugi/base/status/SuccessStatus.java
@@ -0,0 +1,37 @@
+package com.boot.gugi.base.status;
+
+import com.boot.gugi.base.dto.ReasonDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum SuccessStatus implements BaseCode {
+
+    _OK(HttpStatus.OK, "200", "성공입니다."),
+    _CREATED(HttpStatus.CREATED, "201", "생성에 성공했습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    @Override
+    public ReasonDTO getReason() {
+        return ReasonDTO.builder()
+                .isSuccess(true)
+                .code(code)
+                .message(message)
+                .build();
+    }
+
+    @Override
+    public ReasonDTO getReasonHttpStatus() {
+        return ReasonDTO.builder()
+                .isSuccess(true)
+                .httpStatus(httpStatus)
+                .code(code)
+                .message(message)
+                .build();
+    }
+}
