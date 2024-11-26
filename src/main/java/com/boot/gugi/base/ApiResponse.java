93823eb000b783b9a8a772c4172b21ddfce46a31
susooo <92291198+susooo@users.noreply.github.com>
2024-11-26 20:05:30 +0900

    [Feat] : Standardize API responses

---
+++ src/main/java/com/boot/gugi/base/ApiResponse.java
@@ -0,0 +1,41 @@
+package com.boot.gugi.base;
+
+import com.boot.gugi.base.status.BaseCode;
+import com.boot.gugi.base.status.BaseErrorCode;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@Getter
+@RequiredArgsConstructor
+public class ApiResponse<T> {
+    @JsonProperty("isSuccess")
+    private final Boolean isSuccess;
+    private final String code;
+    private final String message;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private final T payload;
+    public static <T> ResponseEntity<ApiResponse<T>> onSuccess(BaseCode code, T payload) {
+        ApiResponse<T> response = new ApiResponse<>(true, code.getReasonHttpStatus().getCode(), code.getReasonHttpStatus().getMessage(), payload);
+        return ResponseEntity.status(code.getReasonHttpStatus().getHttpStatus()).body(response);
+    }
+
+    public static <T> ResponseEntity<ApiResponse<T>> onSuccess(BaseCode code) {
+        ApiResponse<T> response = new ApiResponse<>(true, code.getReasonHttpStatus().getCode(), code.getReasonHttpStatus().getMessage(), null);
+        return ResponseEntity.status(code.getReasonHttpStatus().getHttpStatus()).body(response);
+    }
+
+    public static <T> ResponseEntity<ApiResponse<T>> onFailure(BaseErrorCode code) {
+        ApiResponse<T> response = new ApiResponse<>(false, code.getReasonHttpStatus().getCode(), code.getReasonHttpStatus().getMessage(), null);
+        return ResponseEntity.status(code.getReasonHttpStatus().getHttpStatus()).body(response);
+    }
+
+    public static <T> ResponseEntity<ApiResponse<T>> onFailure(String message, HttpStatus httpStatus) {
+        ApiResponse<T> response = new ApiResponse<>(false, "ERROR", message, null);
+        return ResponseEntity.status(httpStatus).body(response);
+    }
+}
