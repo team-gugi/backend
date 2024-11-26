d7efd366081b5221abd4c06c2e950ad917110b13
susooo <92291198+susooo@users.noreply.github.com>
2024-11-26 20:07:07 +0900

    [Feat] : Add GlobalExceptionHandler

---
+++ src/main/java/com/boot/gugi/token/exception/GlobalExceptionHandler.java
@@ -0,0 +1,68 @@
+package com.boot.gugi.token.exception;
+
+import com.boot.gugi.base.ApiResponse;
+import com.boot.gugi.base.status.BaseErrorCode;
+import com.boot.gugi.exception.UserErrorResult;
+import com.boot.gugi.exception.UserException;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingRequestHeaderException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleException(Exception e) {
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Map<String, String>> handleConstraintViolationException(ConstraintViolationException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", ex.getMessage());
+        logger.error("Constraint violation: {}", ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String, String>> handleIllegalArgumentException(IllegalArgumentException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("message", ex.getMessage());
+        logger.error("Illegal argument: {}", ex.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    // Token
+    @ExceptionHandler(TokenException.class)
+    public ResponseEntity<ApiResponse<BaseErrorCode>> handleTokenException(TokenException e) {
+        TokenErrorResult errorResult = e.getTokenErrorResult();
+        return ApiResponse.onFailure(errorResult);
+    }
+    // User
+    @ExceptionHandler(UserException.class)
+    public ResponseEntity<ApiResponse<BaseErrorCode>> handleUserException(UserException e) {
+        UserErrorResult errorResult = e.getUserErrorResult();
+        return ApiResponse.onFailure(errorResult);
+    }
+    // Header
+    @ExceptionHandler(MissingRequestHeaderException.class)
+    public ResponseEntity<String> handleMissingHeaderException(MissingRequestHeaderException ex) {
+        String errorMessage = "Required header '" + ex.getHeaderName() + "' is missing";
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorMessage);
+    }
+}
