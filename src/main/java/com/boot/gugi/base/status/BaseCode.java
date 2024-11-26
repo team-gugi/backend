5eecf84cdae9bc8ce15b6c04d7828283ce2a96a9
susooo <92291198+susooo@users.noreply.github.com>
2024-11-26 17:17:42 +0900

    [Feat] : Define reason info structure

---
+++ src/main/java/com/boot/gugi/base/status/BaseCode.java
@@ -0,0 +1,8 @@
+package com.boot.gugi.base.status;
+
+import com.boot.gugi.base.dto.ReasonDTO;
+
+public interface BaseCode {
+    public ReasonDTO getReason();
+    public ReasonDTO getReasonHttpStatus();
+}
