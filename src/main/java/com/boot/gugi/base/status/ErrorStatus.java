27b14e27a1c21dcad8fd2fa97a4dc125443ee3dc
susooo <92291198+susooo@users.noreply.github.com>
2024-11-26 17:23:33 +0900

    [Feat] : Add error response codes

---
+++ src/main/java/com/boot/gugi/base/status/ErrorStatus.java
@@ -0,0 +1,39 @@
+package com.boot.gugi.base.status;
+
+import com.boot.gugi.base.dto.ErrorReasonDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorStatus implements BaseErrorCode {
+
+    _INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"500", "서버에서 요청을 처리 하는 동안 오류가 발생했습니다."),
+    _BAD_REQUEST(HttpStatus.BAD_REQUEST,"400", "입력 값이 잘못된 요청 입니다."),
+    _UNAUTHORIZED(HttpStatus.UNAUTHORIZED,"401", "유효하지 않은 인증 토큰입니다."),
+    _FORBIDDEN(HttpStatus.FORBIDDEN, "403", "금지된 요청 입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    @Override
+    public ErrorReasonDTO getReason() {
+        return ErrorReasonDTO.builder()
+                .isSuccess(false)
+                .code(code)
+                .message(message)
+                .build();
+    }
+
+    @Override
+    public ErrorReasonDTO getReasonHttpStatus() {
+        return ErrorReasonDTO.builder()
+                .isSuccess(false)
+                .httpStatus(httpStatus)
+                .code(code)
+                .message(message)
+                .build();
+    }
+}
