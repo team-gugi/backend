c2c51761ccb0030ef36b697da892fb09fa14a864
susooo <92291198+susooo@users.noreply.github.com>
2024-12-16 23:08:33 +0900

    [Feat] : Implement validation check for access token obtained from cookies

--- src/main/java/com/boot/gugi/token/exception/TokenErrorResult.java
+++ src/main/java/com/boot/gugi/token/exception/TokenErrorResult.java
@@ -14,7 +14,8 @@ public enum TokenErrorResult implements BaseErrorCode {
     INVALID_REGISTER_TOKEN(HttpStatus.UNAUTHORIZED, "401", "유효하지 않은 레지스터 토큰입니다."),
     INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "401", "유효하지 않은 액세스 토큰입니다."),
     INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "401", "유효하지 않은 리프레쉬 토큰입니다."),
-    REFRESH_TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "404", "해당 유저 ID의 리프레쉬 토큰이 없습니다.");
+    REFRESH_TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "404", "해당 유저 ID의 리프레쉬 토큰이 없습니다."),
+    ACCESS_TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "404", "쿠키 또는 액세스 토큰이 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
