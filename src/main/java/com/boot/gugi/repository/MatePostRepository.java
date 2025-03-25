ea823d2bced6e06898a4b149610698c07d82d5ef
susooo <92291198+susooo@users.noreply.github.com>
2025-03-26 02:05:19 +0900

    [Refactor]: Updated the logic to mark matePosts as expired instead of deleting them in mypage

--- src/main/java/com/boot/gugi/repository/MatePostRepository.java
+++ src/main/java/com/boot/gugi/repository/MatePostRepository.java
@@ -2,19 +2,19 @@ package com.boot.gugi.repository;
 
 import com.boot.gugi.model.MatePost;
 import com.boot.gugi.model.User;
-import org.springframework.data.domain.Pageable;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
-public interface MatePostRepository extends JpaRepository<MatePost, UUID>, QuerydslPredicateExecutor<MatePost> {
+public interface MatePostRepository extends JpaRepository<MatePost, UUID>, QuerydslPredicateExecutor<MatePost>, MatePostRepositoryCustom {
 
-    List<MatePost> findFirst5ByOrderByUpdatedAtDesc(Pageable pageable);
-    List<MatePost> findByUpdatedAtLessThan(LocalDateTime cursor, Pageable pageable);
     List<MatePost> findAllByUser(User user);
+    @Query("SELECT m FROM MatePost m WHERE m.user = :user AND m.expired = false")
+    List<MatePost> findAllByUserAndNotExpired(@Param("user") User user);
     List<MatePost> findByGameDateBefore(LocalDate date);
 }
--- src/main/java/com/boot/gugi/repository/MateRequestRepository.java
+++ src/main/java/com/boot/gugi/repository/MateRequestRepository.java
@@ -3,7 +3,9 @@ package com.boot.gugi.repository;
 import com.boot.gugi.model.MatePost;
 import com.boot.gugi.model.MateRequest;
 import com.boot.gugi.model.User;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -12,7 +14,8 @@ import java.util.UUID;
 public interface MateRequestRepository extends JpaRepository<MateRequest, UUID> {
 
     boolean existsByApplicantAndMatePost(User applicant, MatePost matePost);
-    List<MateRequest> findAllByApplicant(User applicant);
+    @Query("SELECT mr FROM MateRequest mr WHERE mr.applicant = :user AND mr.matePost.expired = false")
+    List<MateRequest> findAllByApplicantAndNotExpired(@Param("user") User user);
     @Transactional
     void deleteAllByApplicant(User applicant);
     @Transactional
