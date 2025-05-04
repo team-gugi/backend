4d5508ff530785c869ff30f47ac72767c6231184
susooo <92291198+susooo@users.noreply.github.com>
2025-05-04 22:20:16 +0900

    [Fix]: Register JavaTimeModule to support LocalDate serialization - ObjectMapper가 LocalDate를 처리하지 못해 생기는 400 Bad Request 오류 해결

--- src/main/java/com/boot/gugi/base/config/JacksonConfig.java
+++ src/main/java/com/boot/gugi/base/config/JacksonConfig.java
@@ -2,6 +2,8 @@ package com.boot.gugi.base.config;
 
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +14,8 @@ public class JacksonConfig {
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY, true);
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         return objectMapper;
     }
 }
